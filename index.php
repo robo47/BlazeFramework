fd0bb2ccaf8c207fce726afa78b47a949a86ef4e
redshadow159 <redshadow159@1494a6b3-c739-4e9d-80c7-01cfb2d07dc5>
2010-07-26 19:51:44 +0000

    New structure

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-//namespace \;
-include __DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'blaze'.DIRECTORY_SEPARATOR.'lang'.DIRECTORY_SEPARATOR.'Object.php';
-include __DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'blaze'.DIRECTORY_SEPARATOR.'lang'.DIRECTORY_SEPARATOR.'ClassLoader.php';
-
-spl_autoload_register('blaze\lang\ClassLoader::autoLoad');
-\blazeCMS\Main::main($argv != null ? array_shift($argv) : array());
-?>
